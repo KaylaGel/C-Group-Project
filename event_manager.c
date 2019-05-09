f0b9802c1af279bc995feea26e990acb1410d0c5
DjLogozzo <50389807+DjLogozzo@users.noreply.github.com>
2019-05-09 12:59:25 +1000

    Corrected indenting

--- event_manager.c
+++ event_manager.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include "event_manager.h"
 
-    int main()
-    {
-        printf("%i\n", sizeof(event_t));
-        event_t events[MAX_EVENTS];
-        int event_count = 0;
-        return 0;
-    }
+int main(void)
+{
+    event_t events[MAX_EVENTS];
+    int event_count = 0;
+    return 0;
+}
