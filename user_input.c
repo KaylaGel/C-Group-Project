c08d848fcac07e9238dc809da0af89a82b139670
DjLogozzo <50389807+DjLogozzo@users.noreply.github.com>
2019-05-09 20:07:36 +1000

    Removed scan_string and scan_char
    Easier to use scanf() and getchar()

--- user_input.c
+++ user_input.c
@@ -1,23 +1,5 @@
-#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-
-void scan_string(char* input, int len)
-{
-    /* Get one character from stdin and save it to input */
-    fgets(input, len + 1, stdin);
-
-    /* Clear the rest of the stdin buffer */
-    while ((getchar()) != '\n');
-    return;
-}
-
-void scan_char(char* input)
-{
-    scanString(input, 1);
-    return;
-}
 
 int valid_int(char* input)
 {
