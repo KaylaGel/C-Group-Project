e8cb08ca7a09e237a66916a207cf5ead54a8e545
DjLogozzo <50389807+DjLogozzo@users.noreply.github.com>
2019-05-22 13:34:32 +1000

    Added comment describing all the data that needs to be saved

--- add_to_db.c
+++ add_to_db.c
@@ -27,6 +27,27 @@ void add_database(event_t* events, int event_count)
    for (i = 0; i <= event_count; i++)
    {
       fprintf(fp,%s,event[i].name); /*What other variables need to be saved to the file*/
+
+      /*
+       * The username & password of every user
+       * The first & last name of every user
+       * The DOB of every user
+       *
+       * The name of every event
+       * The free/paid status of every event
+       * The event type of every event
+       * Which users are attending for each event
+       * Which users are staff for each event
+       * Which users are coordinators for each event
+       * The number of staff for each event
+       * The number of patrons for each event
+       * The address of each event
+       * The date of each event
+       *
+       * The number of events
+       * The number of users
+       *
+       */
    }
    fclose(fp);
 
