2c70e9ac5c494e80f306a7bea0532f645adadb32
DjLogozzo <50389807+DjLogozzo@users.noreply.github.com>
2019-05-26 18:27:45 +1000

    Re arranged comments in add_to_db.c

--- add_to_db.c
+++ add_to_db.c
@@ -6,11 +6,29 @@
 #define USER_DATABASE_FILE_NAME "Event_Database.txt"
 
 /* To Do */
+
 /*
-  * NOTE xor_encrypt_file() has been created and can be used to xor encrypt files
-  * Could use for the database after it has been saved and before it is loaded    * Could possibly ask user for a database password that is used as the xor key to save and load the database
-  * Wrong password when loading would result in incorrect data loading
-*/
+ * The name of every event
+ * The free/paid status of every event
+ * The event type of every event
+ * Which users are attending for each event
+ * Which users are staff for each event
+ * Which users are coordinators for each event
+ * The number of staff for each event
+ * The number of patrons for each event
+ * The address of each event
+ * The date of each event
+ *
+ * The number of events
+ * The number of users
+ *
+ * NOTE xor_encrypt_file() has been created and can be used to xor encrypt
+ * files
+ * Could use for the database after it has been saved and before it is loaded
+ * Could possibly ask user for a database password that is used as the xor
+ * key to save and load the database
+ * Wrong password when loading would result in incorrect data loading
+*
 
 /************************************************************************
 
@@ -38,22 +56,6 @@ void add_database(event_manager_t* event_manager)
     {
       /* For Event Export might have to change some things due to encryption*/
       fprintf(fp,"%s",event_manager->events[i]); /*What other variables need to be saved to the file*/
-      /*
-       * The name of every event
-       * The free/paid status of every event
-       * The event type of every event
-       * Which users are attending for each event
-       * Which users are staff for each event
-       * Which users are coordinators for each event
-       * The number of staff for each event
-       * The number of patrons for each event
-       * The address of each event
-       * The date of each event
-       *
-       * The number of events
-       * The number of users
-       *
-      */
     }
     
 
